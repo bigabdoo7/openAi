94b33d15d46ce927ab90e13b799482c01d34a4fa
bigabdooè <bigabdoo7@gmail.com>
2019-07-31 15:10:09 +0200

    functional vesion

--- actor_critic.py
+++ actor_critic.py
@@ -7,7 +7,7 @@ import math as m
 # We suppose that our action destribution follows a multivariate normal law, N(mu, sigma), we consider that siqma is constant and thus ont approximate mu(theta).
 #the variable sigma that is used by default is defined below, we check taht no other sigma was specified, if so, the alternative sigma is used.
 if not ('sigma'in locals() or 'sigma'in globals()):
-	sigma = tf.eye(30)*0.25
+	sigma = tf.eye(30)*0.5
 if not ('s_1'in locals() or 's_1'in globals()):
 	s_1 = tf.linalg.inv(sigma)
 pi= tf.constant(m.pi)
@@ -16,21 +16,20 @@ class actor:
 		self.advantage = tf.Variable(1.0, name="advantage", dtype=tf.float32)
 		self.model = keras.Sequential()
 		self.model.add(keras.layers.InputLayer(70))
-		self.model.add(keras.layers.Dense(64, activation="relu"))
+		self.model.add(keras.layers.Dense(256, activation="relu"))
+		self.model.add(keras.layers.Dense(128, activation="relu"))
 		self.model.add(keras.layers.Dense(30, activation="tanh"))
-		sgd= tf.keras.optimizers.SGD(learning_rate=0.001)
+		sgd= tf.keras.optimizers.SGD(learning_rate=0.00001)
 		self.model.compile(optimizer=sgd, loss=self.Loss)
 	def Loss(self, action, mean):
 		mean = tf.reshape(tf.convert_to_tensor(mean), [1, 30])
 		action = tf.reshape(tf.convert_to_tensor(action), [1, 30])
 		return 	 0.5*(self.advantage)*(tf.tensordot(tf.subtract(action, mean), tf.tensordot(s_1 , tf.transpose(tf.subtract(action, mean)), axes=1), axes=1) + (30)*tf.math.log(2*pi) + tf.math.log(tf.linalg.det(sigma)) )
 	def fit(self, x , y, advantage=[[1]]):
-		print("act")
 		self.advantage.assign(advantage[0][0])
-		print(self.advantage)
 		x = tf.reshape(tf.convert_to_tensor(x), [1,70])
 		y = tf.reshape(tf.convert_to_tensor(y), [1,30])
-		self.model.fit(x, y, epochs=5)
+		self.model.fit(x, y, epochs=3, verbose=0)
 	def predict(self, x):
 		x = tf.reshape(tf.convert_to_tensor(x), [1,70])
 		return self.model.predict(x)
@@ -44,15 +43,16 @@ class critic:
 	def __init__(self):
 		self.model = keras.Sequential([
 		keras.layers.InputLayer(70), 
+		keras.layers.Dense(128, activation="relu"),
 		keras.layers.Dense(64, activation="relu"),
 		keras.layers.Dense(1, activation="linear")
 		])
-		self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.01), loss= "mean_squared_error")
+		self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss= "mean_squared_error")
 	def fit(self, x, y):
 		#y is ; r_i + gamma * v(s_{i+1})
 		x = tf.reshape(tf.convert_to_tensor(x), [1,70])
 		y = tf.convert_to_tensor(y)
-		self.model.fit(x, y, epochs=5)
+		self.model.fit(x, y, epochs=3, verbose=0)
 	def predict(self, x):
 		X = tf.reshape(tf.convert_to_tensor(x), [1,70])
 		return self.model.predict(X)
--- atlas1.py
+++ atlas1.py
@@ -8,31 +8,47 @@ from numpy import random
 env = gym.make('RoboschoolAtlasForwardWalk-v1')
 act = ac.actor()
 cri = ac.critic()
-#act.load_weights()
-#cri.load_weights()
+act.load_weights()
+cri.load_weights()
 gamma = 0.95
 tau = []
 ep=0
 ep_count = 0
+sec=0
 avg = 0
+avg_rew=0
+savg = 0
+savg_rew=0
+rew=0
 try:
 	while True:
-		print("episode length= ", ep)
+		if not ep_count % 100 :
+			print("average episode length = {}, and reward {}".format(savg, savg_rew))
+			sec=0
+			act.save_weights()
+			cri.save_weights()
+		if sec != 0:
+			savg = ((sec-1)*savg + ep)/sec
+			savg_rew = ((sec-1)*savg_rew + rew)/sec
 		if ep_count != 0:
 			avg = ((ep_count-1)*avg + ep)/ep_count
+			avg_rew = ((ep_count-1)*avg_rew + rew)/ep_count
 		ep_count += 1
+		sec+=1
 		ep=0
+		rew=0
 		obs= env.reset()
 		while True:
 			ep+=1
 			tau.append(obs)
 			action = act.predict(obs)
-			action = action + random.normal(0.0, 0.25, 30)
+			action = action + random.normal(0.0, 0.5, 30)
 			action = action[0]
 			tau.append(action)
 			obs, reward, done, _ = env.step(action)
 			tau.append(reward)
 			tau.append(obs)
+			rew+=reward
 			if not done:
 				y = tau[2] + gamma * cri.predict(tau[3])
 				adv = y - cri.predict(tau[0])
@@ -48,7 +64,7 @@ try:
 				tau=[]
 				break
 except KeyboardInterrupt:
-	print("\nran {} episodes with average length of {}".format(ep_count, avg))
+	print("\nran {} episodes with average length of {}, and average reward {}".format(ep_count, avg, avg_rew))
 	act.save_weights()
 	cri.save_weights()
 	print("weights saved")
